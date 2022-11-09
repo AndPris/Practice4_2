5b083adc559514a08aaa7aa9454c1fd98299dd9f
Andrii <andpris527@gmail.com>
2022-11-09 20:45:24 +0200

    Check for correct input

--- main.c
+++ main.c
@@ -2,22 +2,46 @@
 
 int main() {
     short n;
+    char ch, cond;
 
-    printf("Length:");
-    if(scanf("%d", &n) <= 0) {
-        printf("Invalid data");
-        return 0;
-    }
+    do {
+        cond = 0;
+        printf("Size of the array:\n");
+        scanf("%hd%c", &n, &ch);
 
+        if(ch != '\n') {
+            printf("Invalid data\n");
+            cond = 1;
+            fflush(stdin);
+            continue;
+        }
+
+        if(n <= 0) {
+            printf("The size of the array can't be lower then 1\n");
+            cond = 1;
+            fflush(stdin);
+        }
+    } while(cond);
+    ch = ' ';
     float arr[n];
+
     printf("Enter the elements of the array\n");
     for(short i = 0; i < n; i++) {
-        scanf("%f", &arr[i]);
+        do {
+            cond = 0;
+            scanf("%f%c", &arr[i], &ch);
+
+            if(ch != '\n') {
+                printf("Invalid data\n");
+                cond = 1;
+                fflush(stdin);
+                continue;
+            }
+        } while(cond);
+        ch = ' ';
     }
 
-    for(short i = 0; i < n; i++) {
-        printf("%f ", arr[i]);
-    }
+
 
 
     return 0;
